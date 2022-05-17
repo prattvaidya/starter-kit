615f716aecbcaec63442ae55373bc8ad4ebec880
Pratt Vaidya <pratt.vaidya@ttt.studio>
2022-05-17 15:37:12 -0700

    Update image for the back and display marker content

--- BusinessCard.js
+++ BusinessCard.js
@@ -40,7 +40,7 @@ export const BusinessCard = () => {
     return (
       <ViroNode>
         <ViroARImageMarker
-          target={'businessCard'}
+          target={'businessCardFront'}
           onAnchorFound={() => setRunAnimation(true)}>
           <ViroNode key="card">
             <ViroNode
@@ -64,7 +64,75 @@ export const BusinessCard = () => {
                   />
                   <ViroText
                     textClipMode="None"
-                    text="Pratt Vaidya"
+                    text="Pratt Vaidya FRONT"
+                    scale={[0.015, 0.015, 0.015]}
+                    style={styles.textStyle}
+                  />
+                </ViroFlexView>
+                <ViroFlexView
+                  // onTouch={() => alert('twitter')}
+                  style={styles.subText}>
+                  <ViroText
+                    width={0.01}
+                    height={0.01}
+                    textAlign="left"
+                    textClipMode="None"
+                    text="@pratttttstudios"
+                    scale={[0.01, 0.01, 0.01]}
+                    style={styles.textStyle}
+                  />
+                  <ViroAnimatedImage
+                    height={0.01}
+                    width={0.01}
+                    loop={true}
+                    source={require('./assets/tweet.gif')}
+                  />
+                </ViroFlexView>
+              </ViroFlexView>
+            </ViroNode>
+            <ViroNode
+              opacity={0}
+              position={[0, 0, 0]}
+              animation={{
+                name: 'animateViro',
+                run: runAnimation,
+              }}>
+              <ViroText
+                text="www.ttt.studio"
+                rotation={[-90, 0, 0]}
+                scale={[0.01, 0.01, 0.01]}
+                style={styles.textStyle}
+              />
+            </ViroNode>
+          </ViroNode>
+        </ViroARImageMarker>
+
+        <ViroARImageMarker
+          target={'businessCardBack'}
+          onAnchorFound={() => setRunAnimation(true)}>
+          <ViroNode key="card">
+            <ViroNode
+              opacity={0}
+              position={[0, -0.02, 0]}
+              animation={{
+                name: 'animateImage',
+                run: runAnimation,
+              }}>
+              <ViroFlexView
+                rotation={[-90, 0, 0]}
+                height={0.03}
+                width={0.05}
+                style={styles.card}>
+                <ViroFlexView style={styles.cardWrapper}>
+                  <ViroImage
+                    height={0.015}
+                    width={0.015}
+                    style={styles.image}
+                    source={require('./assets/avatar.jpg')}
+                  />
+                  <ViroText
+                    textClipMode="None"
+                    text="Pratt Vaidya BACK"
                     scale={[0.015, 0.015, 0.015]}
                     style={styles.textStyle}
                   />
@@ -154,10 +222,17 @@ var styles = StyleSheet.create({
 });
 
 ViroARTrackingTargets.createTargets({
-  businessCard: {
-    source: require('./assets/tttBusinessCard.jpeg'),
+  businessCardFront: {
+    source: require('./assets/tttBusinessCard-front.png'),
     orientation: 'Up',
     physicalWidth: 0.05, // real world width in meters
+    type: 'Image',
+  },
+  businessCardBack: {
+    source: require('./assets/tttBusinessCard-back-template.png'),
+    orientation: 'Up',
+    physicalWidth: 0.05, // real world width in meters
+    type: 'Image',
   },
 });
 
